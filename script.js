825c621a251590d29b3ff63759ff96da9d68fd6a
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-07-01 08:53:31 -0700

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function isSameType(value1, value2) {
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
+if (!isNaN(value1)) {
+  value1 = parseFloat(value1); // Convert to float to handle decimals
+}
+if (!isNaN(value2)) {
+  value2 = parseFloat(value2); // Convert to float to handle decimals
+}
 alert(isSameType(value1, value2));
 
 
